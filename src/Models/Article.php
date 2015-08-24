44c6e86bc8bac7b2653a5716773c96d65cacc58c
Michal Carson <michal.carson@carsonsoftwareengineering.com>
2015-08-24 12:42:23 -0500

    add models

---
+++ src/Models/Article.php
@@ -0,0 +1,10 @@
+<?php namespace NewMarket\Content\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    public function howdy() {
+        return 'howdy';
+    }
+}
